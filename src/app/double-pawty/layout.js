23e5cc2aeca17e85dcc1e9962b2be87cce525626
Nevzat Acar <nacar14@ku.edu.tr>
2024-07-22 09:17:48 +0200

    metadata added for double-pawty

---
+++ src/app/double-pawty/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Double Pawty",
+  description: "Learn the day you and your puppy will be the same age",
+  keywords: ["dog", "dog age", "birthday", "dog age calculator"],
+  openGraph: {
+    images: "./double-pawty/og.png",
+    width: 1200,
+    height: 630,
+  },
+};
+
+export default function RootLayout({ children }) {
+  return <>{children}</>;
+}
